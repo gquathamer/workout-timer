c7eac9095f0aebdfe16c3ef4b43cc607d2590dab
Garrett Quathamer <gquathamer@gmail.com>
2023-04-18 16:05:39 -0600

    updated layout and added a selection component that updates seconds shown in clock

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,5 @@
 import { React, useState, useEffect } from 'react';
+import Input from './input';
 
 export default function App() {
   const [seconds, updateSeconds] = useState(0);
@@ -6,30 +7,34 @@ export default function App() {
 
   useEffect(() => {
     let interval = null;
-
     if (timerRunning) {
       interval = setInterval(() => {
-        updateSeconds(s => s + 1);
+        updateSeconds(s => s - 1);
       }, 1000);
     } else {
       clearInterval(interval);
     }
-
     return () => clearInterval(interval);
-  }, [timerRunning]);
+  }, [timerRunning, seconds]);
 
   function clickMe() {
     setTimerRunning(!timerRunning);
   }
 
   return (
-    <>
-      <div className="row">
-        <div className="clock-container">{seconds}</div>
+    <div className="flex-container">
+      {console.count('render')}
+      <div className="row col-6">
+        <div className="clock-container">
+          <p>{seconds}</p>
+        </div>
+        <div className="full-flex text-center">
+          <button onClick={clickMe}>Click</button>
+        </div>
       </div>
-      <div className="row">
-        <button onClick={clickMe}>Click</button>
+      <div className="row col-6">
+        <Input updateSeconds={updateSeconds}></Input>
       </div>
-    </>
+    </div>
   );
 }
